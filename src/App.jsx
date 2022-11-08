4eb66a9a39dc0a2bda7ea54f38ec9974b869b495
Aline Muller Pasetti <pasettialine@gmail.com>
2022-11-08 16:08:03 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const handleClick = () => {
-    setReverse((reverse) => !reverse);
-  };
+  const [counter2, setCounter2] = useState(0);
 
-  const handleIncrement = () => {
-    setCounter((counter) => counter + 1);
-  };
+  // componentDidUpdate - executa toda vez que o component atualiza
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
+
+  // componentDidMount - executa 1x
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+    console.log('componentDidMount');
+
+    // componentWillUmount - limpeza
+    return () => {
+      console.log('componentWillUnmount');
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  // Com dependência - executa toda vez que a dependência mudar
+  useEffect(() => {
+    console.log('C1:', counter, 'C2:', counter2);
+  }, [counter, counter2]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className={`App-logo ${reverse ? 'reverse' : ''}`}
-          alt="logo"
-        />
-        <button onClick={handleClick}>Reverse {reverse}</button>
-        <br />
-        <button type="button" onClick={handleIncrement}>
-          Increment {counter}
-        </button>
-      </header>
+      <p>Teste 3</p>
+      <h1>
+        C1: {counter} C2: {counter2}{' '}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
     </div>
   );
 }
