6b2d746455e53b698649f52edd1fc12f8113d57b
Aline Muller Pasetti <pasettialine@gmail.com>
2022-11-08 17:37:57 -0300

    useMemo pt 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import P from 'prop-types';
 
-const Button = React.memo(function Button({ incrementButton }) {
+const Button = ({ incrementButton }) => {
   console.log('filho');
   return <button onClick={() => incrementButton(10)}>+</button>;
-});
+};
 
 const App = () => {
   const [counter, setCounter] = useState(0);
@@ -18,7 +18,12 @@ const App = () => {
     <div className="App">
       <p>Teste 3</p>
       <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      {useMemo(
+        () => (
+          <Button incrementButton={incrementCounter} />
+        ),
+        [incrementCounter],
+      )}
     </div>
   );
 };
